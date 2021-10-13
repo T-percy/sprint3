3e39be86f897cf66b072572fe1b2aa67d568af2f
Tonny Percy <tonnypercy@gmail.com>
2021-10-12 22:24:19 -0500

    Agregando methodos HTTP validos

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
     return render_template('pages/inicio.html')
 
@@ -10,34 +10,34 @@ def index():
 def login():
     return render_template('pages/login.html')
 
-@app.route('/registro/')
+@app.route('/registro/', methods=["GET", "POST"])
 def register():
     return render_template('pages/registro.html')
 
-@app.route('/perfil/')
+@app.route('/perfil/', methods=["GET", "POST"])
 def profile():
     return render_template('pages/perfil.html')
 
-@app.route('/dashboard/')
+@app.route('/dashboard/', methods=["GET", "POST"])
 def dashboard():
     return render_template('pages/dashboard.html')
 
-@app.route('/menu/')
+@app.route('/menu/', methods=["GET", "POST"])
 def menu():
     return render_template('pages/menu.html')
 
-@app.route('/plato/')
+@app.route('/plato/', methods=["GET", "POST"])
 def plato():
     return render_template('pages/plato.html')
 
-@app.route('/resultado_busqueda/')
+@app.route('/resultado_busqueda/', methods=["GET"])
 def found():
     return render_template('pages/resultado_busqueda.html')
 
-@app.route('/pedido/')
+@app.route('/pedido/', methods=["GET", "POST"])
 def order():
     return render_template('pages/pedido.html')
 
-@app.route('/lista_deseo/')
+@app.route('/lista_deseo/', methods=["GET", "POST"])
 def list_wish():
     return render_template('pages/lista_deseo.html')
