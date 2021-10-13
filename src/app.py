297b513574728e8a97dc720e7b0b7a0c3da95285
Tonny Percy <tonnypercy@gmail.com>
2021-10-13 18:18:38 -0500

    Validaciones del lado del servidor

--- src/app.py
+++ src/app.py
@@ -1,18 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from flask.json.tag import PassDict
+from markupsafe import escape # Evitar inyección de código
+import os
+
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/', methods=["GET"])
+@app.route('/index/', methods=["GET"])
+@app.route('/inicio/', methods=["GET"])
+@app.route('/home/', methods=["GET"])
 def index():
     return render_template('pages/inicio.html')
 
 @app.route('/login/', methods=["GET", "POST"])
 def login():
-    return render_template('pages/login.html')
+    if request.method=='GET':
+        return render_template('pages/login.html')
+    else:
+        user = escape(request.form["usuario"])#verificar con el formulario
+        password = escape(request.form["contraseña"])#Verificar con el formulario
+    #Validar
+    salida = ''
+    if len(user.strip())<1 or len(user.strip())>8:
+        salida += 'El usuario es requerido y debe tener entre 1 y 8 caracteres'
+    if len(password.strip())<1 or len(password.strip())>8:
+        salida += 'La contraseña es requerida y debe tener entre 1 y 8 caracteres'
+    return redirect('pages/pedido.html')
 
 @app.route('/registro/', methods=["GET", "POST"])
 def register():
-    return render_template('pages/registro.html')
+    if request.method=='GET':
+        return render_template('pages/registro.html')
+    else:
+        nombres = escape(request.form["nombres"]) 
+        apellidos = escape(request.form["apellidos"]) 
+        tipo_identificacion = escape(request.form["tipo_identificacion"]) 
+        identificacion = escape(request.form["identificacion"]) 
+        genero = escape(request.form["genero"]) 
+        direccion = escape(request.form["direccion"]) 
+        pais = escape(request.form["pais"]) 
+        ciudad = escape(request.form["ciudad"]) 
+        telefono = escape(request.form["telefono"]) 
+        fecha_nacimiento = escape(request.form["fecha_nacimiento"]) 
+        acpterminos = escape(request.form["acpterminos"]) 
+    
+    return redirect('pages/login.html')
 
 @app.route('/perfil/', methods=["GET", "POST"])
 def profile():
